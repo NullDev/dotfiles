b4c2f29ee4385515bee83e66268b243eb3dccd9e
NullDev (Shadow) <NL-Dev@yandex.com>
2017-03-01 08:47:39 +0100

    Cleaned up code

--- termcol.sh
+++ termcol.sh
@@ -16,8 +16,14 @@ REVERSE=$(tput smso)
 UNDERLINE=$(tput smul)
 # Might clean that up later
 printf "\n\n"
-printf " ${BLACK}Black${NORMAL}       : BLACK=\$(tput setaf 0)\n ${RED}Red${NORMAL}         : RED=\$(tput setaf 1)\n ${GREEN}Green${NORMAL}       : GREEN=\$(tput setaf 2)\n"
-printf " ${YELLOW}Yellow${NORMAL}      : YELLOW=\$(tput setaf 3)\n ${LIME}Lime${NORMAL}        : LIME=\$(tput setaf 190)\n"
-printf " ${POWDERBLUE}Powder Blue${NORMAL} : POWDERBLUE=\$(tput setaf 153)\n ${BLUE}Blue${NORMAL}        : BLUE=\$(tput setaf 4)\n"
-printf " ${MAGENTA}Magenta${NORMAL}     : MAGENTA=\$(tput setaf 5)\n ${CYAN}Cyan${NORMAL}        : CYAN=\$(tput setaf 6)\n ${WHITE}White${NORMAL}       : WHITE=\$(tput setaf 7)\n\n"
+printf " ${BLACK}Black${NORMAL}       : BLACK=\$(tput setaf 0)\n"
+printf " ${RED}Red${NORMAL}         : RED=\$(tput setaf 1)\n"
+printf " ${GREEN}Green${NORMAL}       : GREEN=\$(tput setaf 2)\n"
+printf " ${YELLOW}Yellow${NORMAL}      : YELLOW=\$(tput setaf 3)\n"
+printf " ${LIME}Lime${NORMAL}        : LIME=\$(tput setaf 190)\n"
+printf " ${POWDERBLUE}Powder Blue${NORMAL} : POWDERBLUE=\$(tput setaf 153)\n"
+printf " ${BLUE}Blue${NORMAL}        : BLUE=\$(tput setaf 4)\n"
+printf " ${MAGENTA}Magenta${NORMAL}     : MAGENTA=\$(tput setaf 5)\n"
+printf " ${CYAN}Cyan${NORMAL}        : CYAN=\$(tput setaf 6)\n"
+printf " ${WHITE}White${NORMAL}       : WHITE=\$(tput setaf 7)\n\n"
 exit 0
