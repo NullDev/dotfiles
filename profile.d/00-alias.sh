ae3e32e333949bc26a3d5687b891d313c721d4c4
NullDev (Shadow) <NL-Dev@yandex.com>
2017-07-13 11:48:44 +0200

    Added alias to read specific line of file

--- profile.d/00-alias.sh
+++ profile.d/00-alias.sh
@@ -7,3 +7,6 @@ alias timer='echo -e " \n################################\n#---     Timer starte
 alias week='date +%V'
 alias gip='curl icanhazip.com'
 alias clearhistory='cat /dev/null > ~/.bash_history && history -c && exit' 
+alias lnn='lnn'
+
+function lnn() { sed -n "$1p" < $2 }
