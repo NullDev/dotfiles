871b8c6f20f34f199e36209b42eb3c88100f17ba
NullDev (Shadow) <NL-Dev@yandex.com>
2017-07-14 11:04:13 +0200

    Added CLS Command + Function

--- profile.d/00-alias.sh
+++ profile.d/00-alias.sh
@@ -8,5 +8,13 @@ alias week='date +%V'
 alias gip='curl icanhazip.com'
 alias clearhistory='cat /dev/null > ~/.bash_history && history -c && exit' 
 alias lnn='lnn'
+alias cls='cls'
 
 function lnn() { sed -n "$1p" < $2 }
+function cls {
+    XA=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+    YA=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+    printf "\033c" 
+    printf "\e[8;${XA};${YA}t" 
+    printf "\e[5t" 
+}
