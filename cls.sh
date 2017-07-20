35d7c0118d136fbcf22e99de2c1d18b8feabf902
NullDev (Shadow) <NL-Dev@yandex.com>
2017-07-20 16:08:07 +0200

    Delete cls.sh

--- cls.sh
+++
@@ -1,7 +0,0 @@
-function cls {
-    XA=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-    YA=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-    printf "\033c" 
-    printf "\e[8;${XA};${YA}t" 
-    printf "\e[5t" 
-}
