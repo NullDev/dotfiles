e407d0da53c5b5f13964c5c63fa05a9b164197e3
NullDev (Shadow) <NL-Dev@yandex.com>
2017-08-23 00:03:04 +0200

    Added arguments

--- forever-start.sh
+++ forever-start.sh
@@ -5,7 +5,7 @@ scr="app.js"
 
 # PATHS
 declare -a arr=(
-        "directory1"
+	"directory1"
         "directory2"
 )
 
@@ -18,36 +18,77 @@ LBLUE=$(tput setaf 153)
 WHITE=$(tput setaf 7)
 NORMAL=$(tput sgr0)
 
-printf "\n\n\n    ${LBLUE}#######################\n"
-printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
-printf "    ${LBLUE}# ${LIME}Starting scripts... ${LBLUE}#\n"
-printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
-printf "    ${LBLUE}#######################${NORMAL}\n\n\n"
+function main {
+	printf "\n\n\n    ${LBLUE}#######################\n"
+	printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
+	printf "    ${LBLUE}# ${LIME}Starting scripts... ${LBLUE}#\n"
+	printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
+	printf "    ${LBLUE}#######################${NORMAL}\n\n\n"
 
-printf "${LBLUE}-----------------------${NORMAL}\n"
+	printf "${LBLUE}-----------------------${NORMAL}\n"
 
-for i in "${arr[@]}"
-do
-        tmp_path="./${i}/${scr}"
-        printf "${LIME}Starting ${RED}${i} ${LIME}at path ${LBLUE}$(pwd)/${i}/${scr}${NORMAL}\n"
-        printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
-        printf "$(forever start -a --minUptime 1000 --spinSleepTime 1000 --uid "${i}" ${tmp_path})"
-        printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
-        printf "${GREEN}Started!${NORMAL}\n"
-        printf "${LBLUE}-----------------------${NORMAL}\n"
-done
+	for i in "${arr[@]}"
+	do
+		tmp_path="./${i}/${scr}"
+		printf "${LIME}Starting ${RED}${i} ${LIME}at path ${LBLUE}$(pwd)/${i}/${scr}${NORMAL}\n"
+		printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		printf "$(forever start -a --minUptime 1000 --spinSleepTime 1000 --uid "${i}" ${tmp_path})"
+		printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		printf "${GREEN}Started!${NORMAL}\n"
+		printf "${LBLUE}-----------------------${NORMAL}\n"
+	done
 
-printf "\n\n    ${LBLUE}#######################\n"
-printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
-printf "    ${LBLUE}# ${LIME}Everything started! ${LBLUE}#\n"
-printf "    ${LBLUE}# ${LIME}Check details below ${LBLUE}#\n"
-printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
-printf "    ${LBLUE}#######################${NORMAL}\n\n\n"
+	printf "\n\n    ${LBLUE}#######################\n"
+	printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
+	printf "    ${LBLUE}# ${LIME}Everything started! ${LBLUE}#\n"
+	printf "    ${LBLUE}# ${LIME}Check details below ${LBLUE}#\n"
+	printf "    ${LBLUE}#${WHITE}---------------------${LBLUE}#\n"
+	printf "    ${LBLUE}#######################${NORMAL}\n\n\n"
 
-printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
-printf "$(forever list)"
-printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
-printf "\n\n\n"
+	printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+	printf "$(forever list)"
+	printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+	printf "\n\n\n"
+
+	exit 0
+}
+
+function init {
+	if [ "${*}" = "--stop" ] || [ "${*}" = "-s" ]; then
+		printf "\n\n\n    ${LBLUE}######################\n"
+		printf "    ${LBLUE}#${WHITE}--------------------${LBLUE}#\n"
+		printf "    ${LBLUE}# ${LIME}Stoping scripts... ${LBLUE}#\n"
+		printf "    ${LBLUE}#${WHITE}--------------------${LBLUE}#\n"
+		printf "    ${LBLUE}######################${NORMAL}\n\n\n"
+
+		printf "${LBLUE}-----------------------${NORMAL}\n"
+
+		printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		printf "$(forever stopall)"
+		printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		exit 0
+	else
+		main
+	fi
+
+	if [ "${*}" = "--restart" ] || [ "${*}" = "-r" ]; then
+		printf "\n\n\n    ${LBLUE}######################\n"
+		printf "    ${LBLUE}#${WHITE}--------------------${LBLUE}#\n"
+		printf "    ${LBLUE}# ${LIME}Stoping scripts... ${LBLUE}#\n"
+		printf "    ${LBLUE}#${WHITE}--------------------${LBLUE}#\n"
+		printf "    ${LBLUE}######################${NORMAL}\n\n\n"
+
+		printf "${LBLUE}-----------------------${NORMAL}\n"
+
+		printf "${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		printf "$(forever stopall)"
+		printf "\n${LIME}+ + + + + + + + + + + +${NORMAL}\n"
+		main
+	else
+		main
+	fi
+}
+
+init
 
 exit 0
-
